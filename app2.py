b27cf118c724c0aa121c076ca1cad861ebfaf905
Thomas Kuenzel <t_kuenzel@gmx.de>
2026-02-12 21:28:23 +0100

    added bootstrap5

--- app2.py
+++ app2.py
@@ -6,9 +6,11 @@ This app deals with jinja templates mostly
 
 # -- IMPORTS
 from flask import Flask, url_for, render_template
+from flask_bootstrap import Bootstrap5
 
 # -- GLOBALS
 app = Flask(__name__)
+bootstrap = Bootstrap5(app)
 
 # -- Routes
 @app.route('/')
