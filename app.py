ab29947a0d92f684c8b14a11148622bece25ed26
Thomas Kuenzel <t_kuenzel@gmx.de>
2026-02-12 20:15:41 +0100

    initial

---
+++ app.py
@@ -0,0 +1,22 @@
+'''
+Docstring for app
+
+This app follows the flask quick start guide
+'''
+
+# IMPORTS
+from flask import Flask
+from markupsafe import escape
+
+# GLOBALS
+app = Flask(__name__)
+
+# Routes
+@app.route("/")
+def helloworld():
+    return "<h1>HELLO WONDERFUL WORLD!</h1>"
+
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "Flask")
+    return f"Hello, {escape(name)}!"
