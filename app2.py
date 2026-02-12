fbd67a029838e83b98301b4c5328433307aba8bc
Thomas Kuenzel <t_kuenzel@gmx.de>
2026-02-12 21:07:41 +0100

    a simple navigation with inheritance from a base

--- app2.py
+++ app2.py
@@ -14,3 +14,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/image')
+def image():
+    return render_template("image.html")
