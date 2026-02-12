1796fac92f70ff2e8d0c21625e1c2bfa248a4dec
Thomas Kuenzel <t_kuenzel@gmx.de>
2026-02-12 20:24:01 +0100

    variable route rules

--- app.py
+++ app.py
@@ -4,19 +4,30 @@ Docstring for app
 This app follows the flask quick start guide
 '''
 
-# IMPORTS
+# -- IMPORTS
 from flask import Flask, request
 from markupsafe import escape
 
-# GLOBALS
+# -- GLOBALS
 app = Flask(__name__)
 
-# Routes
-@app.route("/")
+# -- Routes
+# most basic: return html
+@app.route('/')
 def helloworld():
     return "<h1>HELLO WONDERFUL WORLD!</h1>"
 
-@app.route("/hello")
+# requests sanitizes parameters
+@app.route('/hello')
 def hello():
     name = request.args.get("name", "Flask")
     return f"Hello, {escape(name)}!"
+
+# variable routes
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f"User {escape(username)}"
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f"Post {post_id}"
